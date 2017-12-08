0787ee1d725d7552cc4dabf53de6bd1f4662c553
Anton Bastiuchenko <Anton.Bastiuchenko@infopulse.com>
2017-12-09 01:35:58 +0200

    The first commit

---
+++ COREAzureEventHubConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COREAzureEventHubConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ COREAzureEventHubPublisher/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+
+namespace COREAzureEventHubPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventHubClient client = EventHubClient.CreateFromConnectionString("Endpoint=sb://testgiteventhub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=GotpcwuPgGsRLrKdAqSCdiK/yxcoqGIqrGNzw8yityc=");
+        }
+    }
+}

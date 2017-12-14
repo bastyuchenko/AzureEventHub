a586b60060a735d3a6c929242713893e7ba9033a
Anton Bastiuchenko <Anton.Bastiuchenko@infopulse.com>
2017-12-15 01:25:58 +0200

    EventProcessorHost was fixed

--- FrameworkAzureEventHubConsumer/Program.cs
+++ FrameworkAzureEventHubConsumer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.ServiceBus.Messaging;
 
@@ -11,25 +12,27 @@ namespace FrameworkAzureEventHubConsumer
     {
         static void Main(string[] args)
         {
-            EventHubClient client = EventHubClient.CreateFromConnectionString("Endpoint=sb://ehtest8.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=321/mX76iZUY6GtRBYYp9nzXrspGaS/mP/EZthSteS8=", "ehone");
+//            EventHubClient client = EventHubClient.CreateFromConnectionString("Endpoint=sb://ehtest8.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=321/mX76iZUY6GtRBYYp9nzXrspGaS/mP/EZthSteS8=", "ehone");
 
 
-            EventHubConsumerGroup group = client.GetDefaultConsumerGroup();
-            var receiver = group.CreateReceiver(client.GetRuntimeInformation().PartitionIds[0]);
+//            EventHubConsumerGroup group = client.GetDefaultConsumerGroup();
+//            var receiver = group.CreateReceiver(client.GetRuntimeInformation().PartitionIds[0]);
 
-bool receive = true;
-            string myOffset;
-            while (receive)
-            {
-                var message = receiver.Receive();
-                myOffset = message.Offset;
-                string body = Encoding.UTF8.GetString(message.GetBytes());
-                Console.WriteLine(String.Format("Received message offset: {0} \nbody: {1}", myOffset, body));
-            }
+//bool receive = true;
+//            string myOffset;
+//            while (receive)
+//            {
+//                var message = receiver.Receive();
+//                myOffset = message.Offset;
+//                string body = Encoding.UTF8.GetString(message.GetBytes());
+//                Console.WriteLine(String.Format("Received message offset: {0} \nbody: {1}", myOffset, body));
+//            }
 
-            EventProcessorHost eventProcessorHost = new EventProcessorHost("ehone", EventHubConsumerGroup.DefaultGroupName, "Endpoint=sb://ehtest8.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=321/mX76iZUY6GtRBYYp9nzXrspGaS/mP/EZthSteS8=", "DefaultEndpointsProtocol=https;AccountName=ehstrorage;AccountKey=Swt9Xx2vx98u4riTSHkW0rmFX4tP9mhTZman1YfgczBcdFa0lykdb+hoVWlxL0pyna2O6hkFxjKVd8l5CsOcDA==;EndpointSuffix=core.windows.net");
+            EventProcessorHost eventProcessorHost = new EventProcessorHost("ehph","ehone", EventHubConsumerGroup.DefaultGroupName, "Endpoint=sb://ehtest8.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=321/mX76iZUY6GtRBYYp9nzXrspGaS/mP/EZthSteS8=", "DefaultEndpointsProtocol=https;AccountName=ehstrorage;AccountKey=Swt9Xx2vx98u4riTSHkW0rmFX4tP9mhTZman1YfgczBcdFa0lykdb+hoVWlxL0pyna2O6hkFxjKVd8l5CsOcDA==;EndpointSuffix=core.windows.net");
 
             eventProcessorHost.RegisterEventProcessorAsync<MyEventProcessor>().Wait();
+
+            Thread.Sleep(TimeSpan.FromMinutes(1));
         }
     }
 
@@ -37,19 +40,20 @@ bool receive = true;
     {
         public Task CloseAsync(PartitionContext context, CloseReason reason)
         {
-            return new Task(() => { Console.WriteLine("Process was closed."); });
+            return Task.FromResult<object>(null);
         }
 
         public Task OpenAsync(PartitionContext context)
         {
-            return new Task(() => { Console.WriteLine("Process was started."); });
+            return Task.FromResult<object>(null);
         }
 
         public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
         {
+            
             foreach (var item in messages)
             {
-                Console.WriteLine(Encoding.UTF8.GetString(item.GetBytes()));
+                Console.WriteLine($"{context.Lease.PartitionId }*****{Encoding.UTF8.GetString(item.GetBytes())}");
             }
 
             return Task.FromResult<object>(null);
